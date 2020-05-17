c7eea48e927d862864d145527c480855418bb10d
= <=>
2020-05-17 20:05:32 -0300

    Adicionado a rota do log_sieg

--- backend/src/server/routes.js
+++ backend/src/server/routes.js
@@ -2,6 +2,7 @@ const express = require('express')
 
 const ExtractCompaniesController = require('../controllers/ExtractCompaniesController')
 const SkipsController = require('../controllers/SkipsController')
+const LogSIEGController = require('../controllers/LogSIEGController')
 
 // routes cria as rotas
 const routes = express.Router()
@@ -13,6 +14,9 @@ routes.get('/extract_companies', ExtractCompaniesController.index)
 routes.get('/skips', SkipsController.show)
 routes.post('/skips', SkipsController.store)
 routes.put('/skips', SkipsController.update)
+
+// rotas do log_sieg
+routes.post('/log_sieg', LogSIEGController.store)
 
 // exportando as rotas
 module.exports = routes
