ba59aa948780edd4d401f5b2a57c2b8c07835ba4
= <=>
2020-05-19 20:46:52 -0300

    Adicionado o arquivo que vai abrir vários cmd, um pra cada mês nas exuções do SIEG

---
+++ backend/src/OpensExecutionsCmd.py
@@ -0,0 +1,43 @@
+# coding: utf-8
+
+import os
+import sys
+import json
+from datetime import datetime
+
+absPath = os.path.dirname(os.path.abspath(__file__))
+fileDir = absPath[:absPath.find('backend')]
+sys.path.append(os.path.join(fileDir, 'backend/src/extract'))
+sys.path.append(os.path.join(fileDir, 'backend/src'))
+
+from util.functions import returnCompetenceStartEnd, returnMonthsOfYear
+
+
+class OpenExecutionsCmd():
+    def __init__(self):
+        self._competenceInicial = '01/2019' #input('- Informe a competência inicial (mm/aaaa): ')
+        self._competenceFinal = '05/2020' #input('- Informe a competência final (mm/aaaa): ')
+        self._competenceInicialAndFinal = returnCompetenceStartEnd(self._competenceInicial, self._competenceFinal)
+        self._startMonth = self._competenceInicialAndFinal['monthInicial']
+        self._startYear = self._competenceInicialAndFinal['yearInicial']
+        self._endMonth = self._competenceInicialAndFinal['monthFinal']
+        self._endYear = self._competenceInicialAndFinal['yearFinal']
+
+    def process(self):
+        year = self._startYear
+
+        while year <= self._endYear:
+
+            months = returnMonthsOfYear(year, self._startMonth, self._startYear, self._endMonth, self._endYear)
+
+            for month in months:
+                competence = f'{month:0>2}/{year}'
+                wayFileActual = absPath.replace('\\', '/')
+                command = f"node {wayFileActual}/main/Applicattion.js {competence} {competence}"
+                os.system(f"start cmd /c {command}")
+            
+            year += 1
+
+
+openExecutionsCmd = OpenExecutionsCmd()
+openExecutionsCmd.process()
--- backend/src/util/functions.py
+++ backend/src/util/functions.py
@@ -31,3 +31,35 @@ def readSql(wayFile, nameSql, *args):
         sql = ''
     
     return sql
+
+def returnCompetenceStartEnd(competenceInicial, competenceFinal):
+    objData = {}
+    try:
+        competenceInicialSplit = competenceInicial.split('/')
+        objData['monthInicial'] = int(competenceInicialSplit[0])
+        objData['yearInicial'] = int(competenceInicialSplit[1])
+    except Exception:
+        objData['monthInicial'] = 5
+        objData['yearInicial'] = 2020
+
+    try:
+        competenceFinalSplit = competenceFinal.split('/')
+        objData['monthFinal'] = int(competenceFinalSplit[0])
+        objData['yearFinal'] = int(competenceFinalSplit[1])
+    except Exception:
+        objData['monthFinal'] = 5
+        objData['yearFinal'] = 2020
+    
+    return objData
+
+def returnMonthsOfYear(year, filterMonthStart, filterYearStart, filterMonthEnd, filterYearEnd):
+    if year == filterYearStart and year == filterMonthEnd:
+        months = list(range(filterMonthStart, filterMonthEnd+1)) # o mais 1 é pq o range pega só pega do inicial até o último antes do final, exemplo: range(0,3) = [0,1,2]
+    elif year == filterYearStart:
+        months = list(range(filterMonthStart, 13))
+    elif year == filterYearEnd:
+        months = list(range(1, filterMonthEnd+1))
+    else:
+        months = list(range(1,13))
+
+    return months
